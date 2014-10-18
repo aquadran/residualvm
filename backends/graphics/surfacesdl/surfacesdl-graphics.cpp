5b0b1f8c4e7bc6a8f1edeedbf08e1846247ef2fa
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2014-10-18 09:51:36 +0200

    Added SSE2/SSE4 versions

--- backends/graphics/surfacesdl/surfacesdl-graphics.cpp
+++ backends/graphics/surfacesdl/surfacesdl-graphics.cpp
@@ -536,6 +536,10 @@ void SurfaceSdlGraphicsManager::drawOverlay() {
 	SDL_UnlockSurface(_overlayscreen);
 }
 
+// Based on:
+// http://fastcpp.blogspot.com/2011/06/bilinear-pixel-interpolation-using-sse.html
+// http://tech-algorithm.com/articles/bilinear-image-scaling/
+
 void BlitBilinearScalerFloat(uint16 *dstPtr, int dstW, int dstH, uint16 *srcPtr, int srcW, int srcH) {
 	int a, b, c, d, x, y, index;
 	int offset = 0;
@@ -586,6 +590,9 @@ void BlitBilinearScalerFloat(uint16 *dstPtr, int dstW, int dstH, uint16 *srcPtr,
 	}
 }
 
+// Based on:
+// http://fastcpp.blogspot.com/2011/06/bilinear-pixel-interpolation-using-sse.html
+// http://tech-algorithm.com/articles/bilinear-image-scaling/
 
 void BlitBilinearScalerInteger(uint16 *dstPtr, int dstW, int dstH, uint16 *srcPtr, int srcW, int srcH) {
 	int a, b, c, d, x, y, index;
@@ -637,8 +644,141 @@ void BlitBilinearScalerInteger(uint16 *dstPtr, int dstW, int dstH, uint16 *srcPt
 	}
 }
 
-// Based on http://tech-algorithm.com/articles/bilinear-image-scaling/
+#include <xmmintrin.h>
+#include <smmintrin.h>
+
+// Based on:
+// http://fastcpp.blogspot.com/2011/06/bilinear-pixel-interpolation-using-sse.html
+// http://tech-algorithm.com/articles/bilinear-image-scaling/
+
+static const __m128 CONST_1111 = _mm_set1_ps(1);
+static const __m128 CONST_256 = _mm_set1_ps(256);
+ 
+inline __m128 CalcWeights(float x, float y) {
+	__m128 ssx = _mm_set_ss(x);
+	__m128 ssy = _mm_set_ss(y);
+	__m128 psXY = _mm_unpacklo_ps(ssx, ssy);      // 0 0 y x
+
+#if 0
+// compile with -msse4.1
+	__m128 psXYfloor = _mm_floor_ps(psXY); // use this line for if you have SSE4
+#else
+	_MM_SET_ROUNDING_MODE(_MM_ROUND_TOWARD_ZERO);
+	__m128 psXYfloor = _mm_cvtepi32_ps(_mm_cvtps_epi32(psXY));
+#endif
+	__m128 psXYfrac = _mm_sub_ps(psXY, psXYfloor); // = frac(psXY)
+
+	__m128 psXYfrac1 = _mm_sub_ps(CONST_1111, psXYfrac); // ? ? (1-y) (1-x)
+	__m128 w_x = _mm_unpacklo_ps(psXYfrac1, psXYfrac);   // ? ?     x (1-x)
+	w_x = _mm_movelh_ps(w_x, w_x);      // x (1-x) x (1-x)
+	__m128 w_y = _mm_shuffle_ps(psXYfrac1, psXYfrac, _MM_SHUFFLE(1, 1, 1, 1)); // y y (1-y) (1-y)
+
+	// complete weight vector
+	return _mm_mul_ps(w_x, w_y);
+}
+
 void BlitBilinearScalerSSE(uint16 *dstPtr, int dstW, int dstH, uint16 *srcPtr, int srcW, int srcH) {
+	float x_step, y_step;
+	float x_ratio = ((float)(srcW - 1)) / dstW;
+	float y_ratio = ((float)(srcH - 1)) / dstH;
+	int p[4], index, offset = 0;
+
+	y_step = 0;
+	for (int i = 0; i < dstH; i++) {
+		x_step = 0;
+		int indexBase = (int)y_step * srcW;
+		for (int j = 0;j < dstW; j++) {
+			index = indexBase + (int)x_step;
+			p[0] = (srcPtr[index] & 0xF800) << 8 |
+			    (srcPtr[index] & 0x07E0) << 5 |
+			    (srcPtr[index] & 0x001F) << 3;
+			p[1] = (srcPtr[index + 1] & 0xF800) << 8 |
+			    (srcPtr[index + 1] & 0x07E0) << 5 |
+			    (srcPtr[index + 1] & 0x001F) << 3;
+			p[2] = (srcPtr[index + srcW] & 0xF800) << 8 |
+			    (srcPtr[index + srcW] & 0x07E0) << 5 |
+			    (srcPtr[index + srcW] & 0x001F) << 3;
+			p[3] = (srcPtr[index + srcW + 1] & 0xF800) << 8 |
+			    (srcPtr[index + srcW + 1] & 0x07E0) << 5 |
+			    (srcPtr[index + srcW + 1] & 0x001f) << 3;
+
+			// Load the data (2 pixels in one load)
+			__m128i p12 = _mm_loadl_epi64((const __m128i *)&p[0]);
+			__m128i p34 = _mm_loadl_epi64((const __m128i *)&p[2]);
+
+			__m128 weight = CalcWeights(x_step, y_step);
+#if 0
+// compile with -msse4.1
+
+			// convert RGBA RGBA RGBA RGAB to RRRR GGGG BBBB AAAA (AoS to SoA)
+			__m128i p1234 = _mm_unpacklo_epi8(p12, p34);
+			__m128i p34xx = _mm_unpackhi_epi64(p1234, _mm_setzero_si128());
+			__m128i p1234_8bit = _mm_unpacklo_epi8(p1234, p34xx);
+
+			// extend to 16bit
+			__m128i pRG = _mm_unpacklo_epi8(p1234_8bit, _mm_setzero_si128());
+			__m128i pBA = _mm_unpackhi_epi8(p1234_8bit, _mm_setzero_si128());
+ 
+			// convert weights to integer
+			weight = _mm_mul_ps(weight, CONST_256);
+			__m128i weighti = _mm_cvtps_epi32(weight); // w4 w3 w2 w1
+			weighti = _mm_packs_epi32(weighti, weighti); // 32->2x16bit
+
+			//outRG = [w1*R1 + w2*R2 | w3*R3 + w4*R4 | w1*G1 + w2*G2 | w3*G3 + w4*G4]
+			__m128i outRG = _mm_madd_epi16(pRG, weighti);
+			//outBA = [w1*B1 + w2*B2 | w3*B3 + w4*B4 | w1*A1 + w2*A2 | w3*A3 + w4*A4]
+			__m128i outBA = _mm_madd_epi16(pBA, weighti);
+
+			// horizontal add that will produce the output values (in 32bit)
+			__m128i out = _mm_hadd_epi32(outRG, outBA);
+			out = _mm_srli_epi32(out, 8); // divide by 256
+
+			// convert 32bit->8bit
+			out = _mm_packus_epi32(out, _mm_setzero_si128());
+			out = _mm_packus_epi16(out, _mm_setzero_si128());
+
+			int pixel = _mm_cvtsi128_si32(out);
+#else
+			// extend to 16bit
+			p12 = _mm_unpacklo_epi8(p12, _mm_setzero_si128());
+			p34 = _mm_unpacklo_epi8(p34, _mm_setzero_si128());
+
+			// convert floating point weights to 16bit integer
+			weight = _mm_mul_ps(weight, CONST_256);
+			__m128i weighti = _mm_cvtps_epi32(weight); // w4 w3 w2 w1
+			weighti = _mm_packs_epi32(weighti, _mm_setzero_si128()); // 32->16bit
+
+			// prepare the weights
+			__m128i w12 = _mm_shufflelo_epi16(weighti, _MM_SHUFFLE(1, 1, 0, 0));
+			__m128i w34 = _mm_shufflelo_epi16(weighti, _MM_SHUFFLE(3, 3, 2, 2));
+			w12 = _mm_unpacklo_epi16(w12, w12); // w2 w2 w2 w2 w1 w1 w1 w1
+			w34 = _mm_unpacklo_epi16(w34, w34); // w4 w4 w4 w4 w3 w3 w3 w3
+
+			// multiply each pixel with its weight (2 pixel per SSE mul)
+			__m128i L12 = _mm_mullo_epi16(p12, w12);
+			__m128i L34 = _mm_mullo_epi16(p34, w34);
+
+			// sum the results
+			__m128i L1234 = _mm_add_epi16(L12, L34);
+			__m128i Lhi = _mm_shuffle_epi32(L1234, _MM_SHUFFLE(3, 2, 3, 2));
+			__m128i L = _mm_add_epi16(L1234, Lhi);
+
+			// convert back to 8bit
+			__m128i L8 = _mm_srli_epi16(L, 8); // divide by 256
+			L8 = _mm_packus_epi16(L8, _mm_setzero_si128());
+
+			int pixel = _mm_cvtsi128_si32(L8);
+#endif
+
+			int red   = (pixel >> 16) & 0xFF;
+			int green = (pixel >> 8)  & 0xFF;
+			int blue  = (pixel >> 0)  & 0xFF;
+			dstPtr[offset++] = ((red >> 3) << 11) | ((green >> 2) << 5) | (blue >> 3);
+
+			x_step += x_ratio;
+		}
+		y_step += y_ratio;
+	}
 }
 
 // Based on http://tech-algorithm.com/articles/nearest-neighbor-image-scaling/
